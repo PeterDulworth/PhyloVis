ec9fcbf6d6c34d4059854cdc9dc41979cd95c280
Chabrielle Allen <chab.a.allen@gmail.com>
2017-05-25 12:16:15 -0500

    Added code for creating FASTA sequences

--- Random Seq.py
+++ Random Seq.py
@@ -1,10 +1,11 @@
-## random sequence
+## random sequence generation
 import random
 
-def seq(len, num):
+# PHYLIP Format
+def phylip(len, num):
     """
     Creates random DNA 
-    sequence.
+    sequence in PHYLIP format.
     
     Input: 
     len -- length of sequence
@@ -13,8 +14,8 @@ def seq(len, num):
     Returns:
     Random DNA sequences in a file.
     """
-    file = open("phylip.txt", "w")
     bases = ["A", "T", "C", "G"]
+    file = open("phylip.txt", "w")
     file.write(str(num) + "\n")
     file.write(str(len) + "\n")
     for i in range(num):
@@ -24,4 +25,34 @@ def seq(len, num):
         file.write("\n")
     file.close()
 
-seq(100000000, 10)
+# phylip(100000000, 10)
+
+# FASTA Format
+def fasta(len, num):
+    """
+        Creates random DNA 
+        sequence in FASTA format.
+
+        *** VERY LARGE FILES ***
+        
+        Input: 
+        len -- length of sequence
+        num -- number of sequences
+
+        Returns:
+        Random DNA sequences in a file.
+        """
+    bases = ["A", "T", "C", "G"]
+    file = open("fasta.txt", "w")
+    file.write(str(num) + "\n")
+    file.write(str(len) + "\n")
+    for i in range(num):
+        file.write(">seq" + str(i) + "\n")
+        for j in range(1, len + 1):     # keeps actual length
+            file.write(random.choice(bases))
+            if j % 70 == 0:     # 70-80 lines max
+                file.write("\n")
+        file.write("\n")
+    file.close()
+
+# fasta(100000, 10)
