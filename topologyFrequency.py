3dc63276de4a0a57f69e72ee42e69cfab8dbf908
travis benedict <travisrb10@gmail.com>
2017-06-05 16:25:25 -0500

    Created function for mapping window numbers to topologies

--- topologyFrequency.py
+++ topologyFrequency.py
@@ -101,3 +101,40 @@ def top_topologies(num, directories):
 # dir = ('C:\Users\chaba\GitProjects\PhyloVis', '')
 # top_topologies(5, dir)
 
+def windows_to_topologies(destination_directory):
+    """
+    Maps the name of each window to the newick string representing the topology of the RAxML best tree
+    Inputs:
+    destination_directory --- the folder containing all other outputted folders
+    Output:
+    window_topologies --- a dictionary mapping windows to newick strings
+    """
+
+    windows_topologies = {}
+
+    rax_dir = os.path.join(destination_directory, "RAx_Files")
+
+    # Iterate over each folder in the given directory
+    for filename in os.listdir(rax_dir):
+
+        # If file is the file with the best tree newick string create an image for it
+        if os.path.splitext(filename)[0] == "Topology_bestTree":
+            input_file = os.path.join(rax_dir, filename)
+
+            with open(input_file) as f:
+                # Read newick string from file
+                topology = f.readline()
+                f.close()
+
+                # Map the number of each window to the corresponding newick string
+                window_num = (os.path.splitext(filename)[1]).replace(".","")
+                windows_topologies[window_num] = topology
+
+    return windows_topologies
+
+# Sample run command
+# print windows_to_topologies("C:\\Users\\travi\\Documents\\Evolutionary-Diversity-Visualization-Python")
+
+
+
+
