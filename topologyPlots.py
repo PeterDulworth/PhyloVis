207919aab59992f64abc8a02ea3be945bd502de0
Chabrielle Allen <chab.a.allen@gmail.com>
2017-06-19 11:15:51 -0500

    fixed topologyPlots again

--- topologyPlots.py
+++ topologyPlots.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from Bio.Graphics.GenomeDiagram import GraphSet
 from Bio.Graphics import GenomeDiagram
 from collections import defaultdict
@@ -6,6 +5,7 @@ from reportlab.lib import colors
 import matplotlib.pyplot as plt
 from cStringIO import StringIO
 from natsort import natsorted
+from sys import platform
 from Bio import Phylo
 from PIL import Image
 import numpy as np
@@ -13,7 +13,7 @@ import math
 import os
 
 """
-Functions for creating plots based on the topologies
+Functions for creating plots based on the topologies.
 Chabrielle Allen
 Travis Benedict
 Peter Dulworth
@@ -26,8 +26,9 @@ COLORS = ['#ff0000', '#0000ff', '#ffff00', '#32cd32', '#ba55d3', '#87cefa', '#ff
 
 def topology_counter():
     """
-    Counts the number of times that each topology appears as outputted by RAxML
-    
+    Counts the number of times that each topology appears as outputted by
+    running RAxML.
+
     Output:
     topologies_to_counts --- a dictionary mapping topologies to the number of times they appear
     """
@@ -43,11 +44,9 @@ def topology_counter():
 
         # If file is the file with the best tree newick string
         if os.path.splitext(filename)[0] == "Topology_bestTree":
-
-            input_file = os.path.join(input_directory,filename)
+            input_file = os.path.join(input_directory, filename)
 
             with open(input_file) as f:
-
                 # Read newick string from file
                 topology = f.readline()
 
@@ -62,11 +61,9 @@ def top_freqs(num, topologies_to_counts):
     Makes three lists containing the top 'num' topology
     frequencies and the labels and sizes for the
     donut plot inputs.
-
     Inputs:
     num --- number of topologies to be viewed
     topologies_to_counts --- topologies to counts mapping outputted by topology_counter
-
     Outputs:
     list_of_top_counts --- labels, and sizes
     """
@@ -96,7 +93,7 @@ def top_freqs(num, topologies_to_counts):
 
     # gets topologies less than 'num' most frequent
     if sum(freqs) != 0:
-        labels.append('Other Topologies '+str(len(freqs)))
+        labels.append('Other Topologies ' + str(len(freqs)))
         sizes.append(sum(freqs) / float(total) * 100)
 
     return list_of_top_counts, labels, sizes
@@ -106,11 +103,9 @@ def top_topologies(num, topologies):
     """
     Maps the top 'num' topologies to the number of
     times they occur.
-
     Inputs:
     num        -- number of topologies to analyze
     topologies -- mapping outputted by topology_counter()
-
     Returns:
     top_topologies --- a mapping of the top 'num' topologies
     to the number of times they occur.
@@ -134,18 +129,14 @@ def windows_to_newick(top_topologies):
     Creates a dictionary of window numbers to the topology of that window if
     the newick string contained in the window is a top topology; otherwise the
     window number is mapped to "Other".
-
     Input:
     top_topologies -- a mapping outputted by top_topologies()
-
     Returns:
     wins_to_tops --- a dictionary as described above
     tops_list --- a list of the top topologies
     """
-
-    ###May be possible to optimize this so it doesn't have to iterate over files that aren't Topology_bestTree
+    # Get top topologies and initialize dictionary
     tops_list = top_topologies.keys()
-
     wins_to_tops = {}
 
     # Iterate over each folder in the given directory
@@ -161,7 +152,7 @@ def windows_to_newick(top_topologies):
                 # Read newick string from file
                 newick = f.readline()
 
-            window_number = int((os.path.splitext(filename)[1]).replace(".",""))
+            window_number = int((os.path.splitext(filename)[1]).replace(".", ""))
 
             # Only map windows to newick strings that are in the top topologies
             if newick in tops_list:
@@ -171,6 +162,7 @@ def windows_to_newick(top_topologies):
             else:
 
                 wins_to_tops[window_number] = "Other"
+    # Adds "Other" so all topologies are included with top ones
     tops_list.append("Other")
 
     return wins_to_tops, tops_list
@@ -181,11 +173,9 @@ def topology_colors(wins_to_tops, tops_list):
     Maps topologies to colors and makes two lists
     containing the colors for the scatter plot and
     the y-axis values.
-
     Input:
     wins_to_tops -- mapping outputted by windows_to_newick()[0]
     tops_list    -- list outputted by windows_to_newick()[1]
-
     Returns:
     A mapping tops_to_colors and two lists scatter_colors and
     ylist.
@@ -204,7 +194,7 @@ def topology_colors(wins_to_tops, tops_list):
     # create list of colors of same length as number of windows
     top_colors = COLORS[:len(ylist)]
 
-    # map colors to topologies so they are the same in the plot
+    # map colors to topologies so they are the same in scatter plot
     for win in wins_to_tops:
         if wins_to_tops[win] in tops_to_colors.keys():
             scatter_colors.append(tops_to_colors[wins_to_tops[win]])
@@ -220,17 +210,16 @@ def donut_colors(top_topologies, tops_to_colors):
     """
     Makes a color list formatted for use in the donut chart
     so that it matches the scatter plot.
-
     Input:
     top_topologies -- mapping outputted by top_topologies()
     tops_to_colors -- mapping outputted by tops_to_colors()
-
     Returns:
     A list donut_colors.
     """
     # initialize color list
     donut_colors = []
 
+    # sort topologies based on number of occurrences (high to low)
     tops = top_topologies.items()
     topologies = sorted(tops, key=lambda tup: tup[1], reverse=True)
 
@@ -252,7 +241,6 @@ def topology_donut(num, top, labels, sizes, donut_colors):
     """
     Creates a donut chart showing the breakdown of the top 'num'
     topologies.
-
     Inputs:
     num    -- the number of topologies to be shown
     top    -- a list of the top frequencies outputted by
@@ -261,7 +249,6 @@ def topology_donut(num, top, labels, sizes, donut_colors):
     sizes  -- a list of sizes outputted by top_freqs()[2]
     donut_colors -- a list of colors outputted by
                     donut_colors()
-
     Returns:
     A donut chart with the number of times a topology occurs and
     'Other Topologies' for topologies that occur less than the
@@ -270,7 +257,7 @@ def topology_donut(num, top, labels, sizes, donut_colors):
     """
     # plots pie chart
     plt.pie(sizes, explode=None, labels=labels,
-        colors=donut_colors, autopct=None, shadow=False)
+            colors=donut_colors, autopct=None, shadow=False)
 
     # impose circle over pie chart to make a donut chart
     circle = plt.Circle((0, 0), 0.65, color='black', fc='white',
@@ -290,22 +277,18 @@ def topology_scatter(wins_to_tops, scatter_colors, ylist):
     """
     Creates a scatter plot showing the topology as the
     y-axis and the window as the x-axis.
-
     Input:
     wins_to_tops   -- window to topology mapping outputted by windows_to_newick()[0]
     scatter_colors -- list of colors outputted by topology_colors()[1]
     ylist          -- list of y-axis values outputted by topology_colors()[2]
-
     Returns:
     A scatter plot with topologies as the x-axis and
     windows as the y-axis.
     """
     # area of plotted circles
-    area = math.pi * (3)**2
+    area = math.pi * (3) ** 2
 
-    # sizes plot appropriately
-    # plt.xticks(np.arange(0, len(wins_to_tops) + 1, 1.0))
-    # plt.yticks(np.arange(0, len(wins_to_tops) + 1, 1.0))
+    # size y-axis on plot
     plt.yticks(np.arange(len(wins_to_tops) + 1, 0))
 
     # x-axis is window numbers
@@ -326,10 +309,9 @@ def topology_scatter(wins_to_tops, scatter_colors, ylist):
     # labels axes
     plt.xlabel('Windows', fontsize=10)
     plt.ylabel('Top Newick Strings', fontsize=10)
-    # plt.show()
 
-    # Save plot
-    plot = "topologyPlot.png"
+    # save plot
+    plot = "topologyScatter.png"
     plt.savefig(plot)
     plt.clf()
 
@@ -338,13 +320,13 @@ def topology_colorizer(color_scheme):
     """
     Create colored tree topology images based on a color scheme where
     the color of a tree is determined by the frequency that it occurs.
-
     Inputs:
     color scheme --- a dictionary mapping newick strings to colors
     """
 
     # Create a count for the number of the topologies
     count = 0
+
     # Iterate over each newick string in color_scheme
     for newick in color_scheme:
 
@@ -373,33 +355,36 @@ def topology_colorizer(color_scheme):
             count += 1
 
 
-def generateCircleGraph(file, windows_to_top_topologies, top_topologies_to_colors, window_size, window_offset):
+def generateCircleGraph(file, windows_to_top_topologies, topologies_to_colors, window_size, window_offset):
     """
     Creates genetic circle graph showing the windows and the areas where each topology appears
-
     Inputs:
-    file --- 
-    windows_to_top_topologies ---
-    top_topologies_to_colors --- 
-    window_size --- 
-    window_offset ---
+    file --- phylip file inputted in GUI
+    windows_to_top_topologies --- mapping outputted by windows_to_newick()[0]
+    topologies_to_colors --- mapping outputted by topology_colors()[0]
+    window_size --- size inputted in GUI
+    window_offset --- size inputted in GUI
+    Returns:
+    A genetic circle graph GenomeAltase.
     """
 
     ############################# Format Data #############################
 
     # -1 because top_topologies_to_colors includes 'Other'
-    number_of_top_topologies = len(top_topologies_to_colors) - 1
+    number_of_top_topologies = len(topologies_to_colors) - 1
 
     # get the length of the sequence
     with open(file) as f:
         length_of_sequences = int(f.readline().split()[1])
     f.close()
 
+    # accounts for offset
     windows_to_top_topologies2 = {}
     for window in windows_to_top_topologies:
         windows_to_top_topologies2[window * window_offset] = windows_to_top_topologies[window]
     windows_to_top_topologies = windows_to_top_topologies2.items()
 
+    # gets windows
     windows = []
     for window_topology in windows_to_top_topologies:
         windows.append(window_topology[0])
@@ -408,26 +393,29 @@ def generateCircleGraph(file, windows_to_top_topologies, top_topologies_to_color
         if i not in windows:
             windows_to_top_topologies.append((i, 0))
 
+    # maps data to topologies
     topologies_to_data = {}
-
-    for topology in top_topologies_to_colors:
+    for topology in topologies_to_colors:
         topologies_to_data[topology] = []
 
-    for topology in top_topologies_to_colors:
+    for topology in topologies_to_colors:
         for window in windows_to_top_topologies:
             if topology == window[1]:
                 topologies_to_data[topology].append(tuple([window[0], 1]))
             else:
                 topologies_to_data[topology].append(tuple([window[0], -0.1]))
 
+    # maps data to colors
     data_to_colors = {}
     for topology in topologies_to_data:
-        data_to_colors[str(topologies_to_data[topology])] = top_topologies_to_colors[topology]
+        data_to_colors[str(topologies_to_data[topology])] = topologies_to_colors[topology]
 
+    # removes 'Other' from mapping
     minor_topology_data = topologies_to_data['Other']
     del topologies_to_data['Other']
     data = topologies_to_data.values()
 
+    # separates data into windowed and unwindowed
     windowed_data = []
     nonwindowed_data = []
     for i in range(length_of_sequences):
@@ -442,7 +430,7 @@ def generateCircleGraph(file, windows_to_top_topologies, top_topologies_to_color
     ############################# Build Graph #############################
 
     # name of the figure
-    name = "circleGraph"
+    name = "GenomeAltase"
     graphStyle = 'bar'
 
     # create the diagram -- highest level container for everything
@@ -522,11 +510,10 @@ def generateCircleGraph(file, windows_to_top_topologies, top_topologies_to_color
 
 if __name__ == '__main__':
     # User inputs:
-    num = 2
-    # file = 'phylip.txt'
-    file = "ChillLeo.phylip"
-    windowSize = 50000
-    windowOffset = 50000
+    num = 3
+    file = 'phylip.txt'
+    windowSize = 10
+    windowOffset = 10
 
     # Function calls for plotting inputs:
     topologies_to_counts = topology_counter()
@@ -546,11 +533,12 @@ if __name__ == '__main__':
     topology_donut(num, list_of_top_counts, labels, sizes, donut_colors)
     topology_colorizer(topologies_to_colors)
 
-    # generateCircleGraph(file, windows_to_top_topologies, topologies_to_colors, windowSize, windowOffset)
+    generateCircleGraph(file, windows_to_top_topologies, topologies_to_colors, windowSize, windowOffset)
 
     if platform == "win32":
-        os.startfile("circleGraph" + ".pdf")
+        os.startfile("GenomeAltase" + ".pdf")
 
     elif platform == "darwin":
-        os.system("open " + "circleGraph" + ".pdf")
+        os.system("open " + "GenomeAltase" + ".pdf")
+
 
