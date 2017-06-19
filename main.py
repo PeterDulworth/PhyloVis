8280a2adad33c38aacd096851bfa92b9398556a7
Peter Dulworth <pdulworth97@gmail.com>
2017-06-19 10:55:04 -0500

    fixed travys issue about erro handling if statistics checkbox is not checked

--- main.py
+++ main.py
@@ -275,36 +275,37 @@ class PhyloVisApp(QtGui.QMainWindow, gui.Ui_PhylogeneticVisualization):
     def run(self):
 
         # Error handling for newick file
-        try:
-            self.newickFileName = str(self.newickFileEntry.text())
-            self.newickFileExtension = os.path.splitext(self.newickFileName)[1]
+        if self.checkboxProbability.isChecked():
+            try:
+                self.newickFileName = str(self.newickFileEntry.text())
+                self.newickFileExtension = os.path.splitext(self.newickFileName)[1]
 
-            self.newickStringFromEntry = str(self.speciesTreeNewickStringsEntry.text())
+                self.newickStringFromEntry = str(self.speciesTreeNewickStringsEntry.text())
 
-            if self.newickFileName == "" and self.newickStringFromEntry == "":
-                raise ValueError, (1, "Please choose a file or enter a newick string")
-            elif self.newickFileName != "" and self.newickStringFromEntry != "":
-                raise ValueError, (2, "You have chosen a file and entered a newick string. Please choose one.")
-            # elif self.newickFileExtension != '.txt' and self.newickFileExtension != '.phylip' and self.newickFileExtension != '.fasta' and self.newickFileName != '':
-            #     raise ValueError, (
-            #     3, "Luay does not approve of your filetype.\nPlease enter either a .txt, .fasta, or .phylip file")
+                if self.newickFileName == "" and self.newickStringFromEntry == "":
+                    raise ValueError, (1, "Please choose a file or enter a newick string")
+                elif self.newickFileName != "" and self.newickStringFromEntry != "":
+                    raise ValueError, (2, "You have chosen a file and entered a newick string. Please choose one.")
+                # elif self.newickFileExtension != '.txt' and self.newickFileExtension != '.phylip' and self.newickFileExtension != '.fasta' and self.newickFileName != '':
+                #     raise ValueError, (
+                #     3, "Luay does not approve of your filetype.\nPlease enter either a .txt, .fasta, or .phylip file")
 
-            # get checkbox values
-            self.robinsonFoulds = self.checkboxRobinsonFoulds.isChecked()
-            self.weighted = self.checkboxWeighted.isChecked()
-            self.pgtst = self.checkboxProbability.isChecked()
+                # get checkbox values
+                self.robinsonFoulds = self.checkboxRobinsonFoulds.isChecked()
+                self.weighted = self.checkboxWeighted.isChecked()
+                self.pgtst = self.checkboxProbability.isChecked()
 
-            # if the newick input is from the file chooser
-            if self.newickFileName != '':
-                with open(self.newickFileEntry.text(), 'r') as f:
-                    self.speciesTree = f.read().replace('\n', '')
-            # else if the newick input is from the manual text entry
-            elif self.newickStringFromEntry != '':
-                self.speciesTree = str(self.speciesTreeNewickStringsEntry.text())
+                # if the newick input is from the file chooser
+                if self.newickFileName != '':
+                    with open(self.newickFileEntry.text(), 'r') as f:
+                        self.speciesTree = f.read().replace('\n', '')
+                # else if the newick input is from the manual text entry
+                elif self.newickStringFromEntry != '':
+                    self.speciesTree = str(self.speciesTreeNewickStringsEntry.text())
 
-        except ValueError, (ErrorNumber, ErrorMessage):
-            QtGui.QMessageBox.about(self, "Invalid Input", str(ErrorMessage))
-            return
+            except ValueError, (ErrorNumber, ErrorMessage):
+                QtGui.QMessageBox.about(self, "Invalid Input", str(ErrorMessage))
+                return
 
         # Error handling for input file
         try:
