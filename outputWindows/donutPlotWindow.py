fcedb824ac34a09eb899551300c47f22d0e3751b
Peter Dulworth <pdulworth97@gmail.com>
2017-06-21 15:02:12 -0500

    added export to donut plot

--- outputWindows/donutPlotWindow.py
+++ outputWindows/donutPlotWindow.py
@@ -1,13 +1,23 @@
 from PyQt4 import QtGui, QtCore
 import donutPlotLayout
-import sys
 from PIL import Image
+import sys, os
+from shutil import copyfile
 
 
-class DonutPlotWindow(QtGui.QWidget, donutPlotLayout.Ui_donutPlot):
+class DonutPlotWindow(QtGui.QMainWindow, donutPlotLayout.Ui_donutPlot):
     def __init__(self, parent=None):
         super(DonutPlotWindow, self).__init__(parent)
         self.setupUi(self)
+
+        self.fileName = 'topologyDonut.png'
+
+        # moves menu bar into application -- mac only windows sux
+        self.menubar.setNativeMenuBar(False)
+
+        # bind export actions
+        self.actionPNG.triggered.connect(lambda: self.exportFile(self.fileName))
+        self.actionPDF.triggered.connect(lambda: self.exportFile(self.fileName))
 
     def display_image(self):
         # image = Image.open('../topologyDonut.png')
@@ -16,10 +26,22 @@ class DonutPlotWindow(QtGui.QWidget, donutPlotLayout.Ui_donutPlot):
         # print size
         # image.save('../test.png', 'PNG', quality=200)
 
+        # change background color to white
+        p = self.palette()
+        p.setColor(self.backgroundRole(), QtCore.Qt.white)
+        self.setPalette(p)
+
+        # get size of image
+
         self.move(0, 600)
-        self.donutPixmap = QtGui.QPixmap("topologyDonut.png").scaled(600, 600, QtCore.Qt.KeepAspectRatio)
+        self.donutPixmap = QtGui.QPixmap(self.fileName).scaled(600, 600, QtCore.Qt.KeepAspectRatio)
         self.donutPlotImage.setScaledContents(False)
         self.donutPlotImage.setPixmap(self.donutPixmap)
+
+    def exportFile(self, fileName):
+        extension = os.path.splitext(fileName)[1]
+        name = QtGui.QFileDialog.getSaveFileName(self, 'Export ' + extension[1:]) + extension
+        copyfile(fileName, name)
 
 # if you want to test LOCALLY change the path to ../topologyDonut.png #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 if __name__ == '__main__':  # if we're running file directly and not importing it
