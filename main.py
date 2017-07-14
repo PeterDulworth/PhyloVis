c8c04fc2b4f989afeac8f8682d4724c5dcb8735f
Peter Dulworth <pdulworth97@gmail.com>
2017-07-14 16:17:39 -0500

    err

--- main.py
+++ main.py
@@ -408,16 +408,16 @@ class PhyloVisApp(QtGui.QMainWindow, gui.Ui_PhylogeneticVisualization):
                 self.pgtstWindow.displayImage()
 
     def generateSpeciesTree(self):
-        # Error handling for input file
         try:
+            # get values from gui -- ensure that no fields are blank
             self.raxmlOperations.inputFilename = self.checkEntryPopulated(self.inputFileEntry, errorTitle='', errorMessage='')
             self.raxmlOperations.windowSize = self.checkEntryInRange(self.windowSizeEntry, min=0, inclusive=False, errorTitle='Invalid Window Size', errorMessage='Window size needs to be a positive integer.')
             self.raxmlOperations.windowOffset = self.checkEntryInRange(self.windowOffsetEntry, min=0, inclusive=False, errorTitle='Invalid Window Offset', errorMessage='Window offset needs to be a positive integer.')
-
             self.raxmlOperations.speciesTreeRooted = self.checkboxSpeciesTreeRooted.isChecked()
             self.raxmlOperations.speciesTreeOutGroup = self.speciesTreeComboBox.currentText()
             self.raxmlOperations.speciesTreeUseCustomRax = self.checkboxSpeciesTreeUseCustomRax.isChecked()
 
+            # if using custom rax -- make sure that the user doesn't use the -s or -n flags
             if self.checkboxSpeciesTreeUseCustomRax.isChecked():
                 self.raxmlOperations.speciesTreeCustomRaxmlCommand = self.checkEntryPopulated(self.speciesTreeRaxmlCommandEntry, errorTitle='No RAxML Command', errorMessage='Please enter a custom raxml command or uncheck the box.')
                 if re.search('([\-][n])|([\-][s])', self.speciesTreeRaxmlCommandEntry.text()):
@@ -504,24 +504,12 @@ class PhyloVisApp(QtGui.QMainWindow, gui.Ui_PhylogeneticVisualization):
             self.raxmlOperations.windowSize = self.checkEntryInRange(self.windowSizeEntry, min=0, inclusive=False, errorTitle='Invalid Window Size', errorMessage='Window size needs to be a positive integer.')
             self.raxmlOperations.windowOffset = self.checkEntryInRange(self.windowOffsetEntry, min=0, inclusive=False, errorTitle='Invalid Window Offset', errorMessage='Window offset needs to be a positive integer.')
             self.topTopologies = self.checkEntryInRange(self.numberOfTopTopologiesEntry, min=0, max=16, inclusive=False, errorTitle='Invalid Number of Top Topologies', errorMessage='Please enter an integer between 0 and 15.')
-
-            # statistics error handling
-            if self.checkboxRobinsonFoulds.isChecked() or self.checkboxPGTST.isChecked():
-                self.newickFileName = str(self.newickFileEntry.text())
-                self.newickStringFromEntry = str(self.speciesTreeNewickStringsEntry.text())
-
-                if self.newickFileName == "" and self.newickStringFromEntry == "":
-                    raise ValueError, ("Select Species Tree", "You must either enter a newick string or select a file.", "The 'Species Tree File' and the 'Species Tree Newick' inputs are both blank. Please enter one.")
-                elif self.newickFileName != "" and self.newickStringFromEntry != "":
-                    raise ValueError, ("Too Many Inputs", "You have chosen a file and entered a newick string. Please choose ONLY one.", None)
-
-                # if the newick input is from the file chooser
-                if self.newickFileName != '':
-                    with open(self.newickFileEntry.text(), 'r') as f:
-                        self.speciesTree = f.read().replace('\n', '')
-                # else if the newick input is from the manual text entry
-                elif self.newickStringFromEntry != '':
-                    self.speciesTree = str(self.speciesTreeNewickStringsEntry.text())
+            self.raxmlOperations.outGroup = self.outgroupComboBox.currentText()
+            self.raxmlOperations.model = self.modelComboBox.currentText()
+            self.raxmlOperations.isCustomRaxmlCommand = self.checkBoxCustomRaxml.isChecked()
+            self.raxmlOperations.bootstrap = self.checkboxBootstrap.isChecked()
+            self.raxmlOperations.rooted = self.checkboxRooted.isChecked()
+            self.rooted = self.checkboxRooted.isChecked()
 
             # bootstrap error handling
             self.raxmlOperations.numBootstraps = 0
@@ -529,18 +517,17 @@ class PhyloVisApp(QtGui.QMainWindow, gui.Ui_PhylogeneticVisualization):
                 self.confidenceLevel = self.checkEntryInRange(self.confidenceLevelEntry, min=0, max=100, errorTitle='Invalid Confidence Level', errorMessage='Please enter an integer between 0 and 100.')
                 self.raxmlOperations.numBootstraps = self.checkEntryInRange(self.numberOfBootstrapsEntry, min=2, errorTitle='Invalid Number of Bootstraps', errorMessage='Please enter an integer greater than 1.')
 
-            # custom raxml error handling
+            # if using custom rax -- make sure that the user doesn't use the -s or -n flags
             if self.checkBoxCustomRaxml.isChecked():
+                self.raxmlOperations.customRaxmlCommand = self.checkEntryPopulated(self.customRaxmlCommandEntry, errorTitle='No RAxML Command', errorMessage='Please enter a custom raxml command or uncheck the box.')
                 if re.search('([\-][n])|([\-][s])', self.customRaxmlCommandEntry.text()):
                     raise ValueError, ('Invalid RAxML Command', 'Please do not specify the -s or -n flags.', 'the -s and -n flags will be handled internally based on the alignment you input.')
-                self.raxmlOperations.customRaxmlCommand = self.checkEntryPopulated(self.customRaxmlCommandEntry, errorTitle='No RAxML Command', errorMessage='Please enter a custom raxml command or uncheck the box.')
 
-            self.raxmlOperations.outGroup = self.outgroupComboBox.currentText()
-            self.raxmlOperations.model = self.modelComboBox.currentText()
-            self.raxmlOperations.isCustomRaxmlCommand = self.checkBoxCustomRaxml.isChecked()
-            self.raxmlOperations.bootstrap = self.checkboxBootstrap.isChecked()
-            self.raxmlOperations.rooted = self.checkboxRooted.isChecked()
-            self.rooted = self.checkboxRooted.isChecked()
+            # statistics error handling
+            if self.checkboxRobinsonFoulds.isChecked() or self.checkboxPGTST.isChecked():
+                self.newickFileName = self.checkEntryPopulated(self.newickFileEntry)
+                with open(self.newickFileEntry.text(), 'r') as f:
+                    self.speciesTree = f.read().replace('\n', '')
 
         except ValueError, (ErrorTitle, ErrorMessage, ErrorDescription):
             self.message(str(ErrorTitle), str(ErrorMessage), str(ErrorDescription))
@@ -576,10 +563,6 @@ class PhyloVisApp(QtGui.QMainWindow, gui.Ui_PhylogeneticVisualization):
         # default pixmap for error
         pixmap = QtGui.QPixmap('imgs/warning.png')
 
-        # choose icon based on type
-        if type == 'testType2':
-            pixmap = 'imgs/tree.png'
-
         # set icon
         errMessage.setIconPixmap(pixmap)
 
@@ -592,12 +575,11 @@ class PhyloVisApp(QtGui.QMainWindow, gui.Ui_PhylogeneticVisualization):
                 (i) if entry is populated returns text
                 (ii) otherwise raises value error
         """
-        text = entry.text()
-        print 'poop'
+        text = str(entry.text())
+
         if text == '':
-            print 'poop2'
             raise ValueError(errorTitle, errorMessage, errorDescription)
-            print 'poop3'
+
         return text
 
     def checkEntryInRange(self, entry, min=(-1.0 * float('inf')), max=float('inf'), inclusive=True, errorTitle='rip', errorMessage='rip', errorDescription='n/a'):
