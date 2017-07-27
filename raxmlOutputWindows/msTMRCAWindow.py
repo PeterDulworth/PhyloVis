1d98a25293d5f7a3aa3cf02d877ae3b6c5880361
Peter Dulworth <pdulworth97@gmail.com>
2017-07-27 12:02:34 -0500

    fixed species tree, also gui is new

--- raxmlOutputWindows/msTMRCAWindow.py
+++ raxmlOutputWindows/msTMRCAWindow.py
@@ -24,9 +24,10 @@ if __name__ == '__main__': # only runs if not imported
     app = QtGui.QApplication(sys.argv)
 
     a = {0:0, 2:2, 4:3}
+    b = [1,2,3]
 
     # create window and plot
-    form = MSTMRCAWindow(a)
+    form = MSTMRCAWindow(a, b)
 
     # execute the app
     sys.exit(app.exec_())
