7ecfe48604dacebfadf7c2b1a57555ec43d34221
travisbenedict <trb6@rice.edu>
2017-07-27 13:45:41 -0500

    Resized RAxML window for Windows

--- main.py
+++ main.py
@@ -85,7 +85,7 @@ class PhyloVisApp(QtGui.QMainWindow, gui.Ui_PhylogeneticVisualization):
         # if running windows os
         if sys.platform == 'win32':
             # mapping from: windows --> dictionary of page dimensions
-            self.windowSizes = {'welcomePage': {'x': 459, 'y': 245}, 'inputPageRax': {'x': 871, 'y': 688}, 'inputPageFileConverter': {'x': 630, 'y': 375}, 'inputPageMS': {'x': 675, 'y': 815}, 'inputPageDStatistic': {'x': 600, 'y': 570}}
+            self.windowSizes = {'welcomePage': {'x': 459, 'y': 245}, 'inputPageRax': {'x': 925, 'y': 688}, 'inputPageFileConverter': {'x': 630, 'y': 375}, 'inputPageMS': {'x': 675, 'y': 815}, 'inputPageDStatistic': {'x': 600, 'y': 570}}
 
         # initialize window
         self.circleGraphWindow = circleGraphWindow.CircleGraphWindow()
